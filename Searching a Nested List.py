9f8da1f9a798edb870794e1baab6ff05c1dcc5e4
Kian Shirazi <kianshirazi@MBP-von-Samsung.fritz.box>
2022-10-21 22:22:34 +0200

    got the code running
    missing feature is described in the code (running on nested lists)

--- Searching a Nested List.py
+++ Searching a Nested List.py
@@ -6,6 +6,20 @@ def nestedListSum(NL):
         sum = sum + nestedListSum(NL[i])
     return sum
 
-def nestedListContains(NL, target): 
-    #this is test comment
-    return "Hello, World!"
+def nestedListContains(NL, target):
+    if isinstance(NL, int) and NL == target: return True # case1: only one int item as input and equal to target
+    elif isinstance(NL, int) and NL != target: return False # case2: only one int item as input but unequal to target
+    
+    for i in range(0, len(NL)): 
+         if NL[i] == target: return True 
+         else: nestedListContains(NL[i], target)
+    return False
+
+# current status: the code is working for lists. The problem we have now is to get it running for lists inside of other lists...
+
+# Test section
+print(nestedListContains([1, [9, 2], 3], 3))
+# print(nestedListSum([3]))
+
+#var = [1, 2, 3, 4]
+# print(isinstance(var, int)) # Is instance is only used for single numbers 
