1fe2bece0f2561767bea9b2862a4b12a99a530a0
Kian Shirazi <kianshirazi@MBP-von-Samsung.fritz.box>
2022-11-09 18:11:58 +0100

    Fixed & completed the exercise with help from Simon.

--- Searching a Nested List.py
+++ Searching a Nested List.py
@@ -8,17 +8,17 @@ def nestedListSum(NL):
 
 def nestedListContains(NL, target):
     if isinstance(NL, int) and NL == target: return True # case1: only one int item as input and equal to target
-    elif isinstance(NL, int) and NL != target: return False # case2: only one int item as input but unequal to target
+    if isinstance(NL, int) and NL != target: return False # case2: only one int item as input but unequal to target
     
     for i in range(0, len(NL)): 
-         if NL[i] == target: return True 
-         else: nestedListContains(NL[i], target)
+        containsTarget = nestedListContains(NL[i], target)
+        if containsTarget: 
+            return True
     return False
 
-# current status: the code is working for lists. The problem we have now is to get it running for lists inside of other lists...
 
 # Test section
-print(nestedListContains([1, [9, 2], 3], 3))
+print(nestedListContains([1, 2, 3, [4, 5, 2, [8]]], 8))
 # print(nestedListSum([3]))
 
 #var = [1, 2, 3, 4]
